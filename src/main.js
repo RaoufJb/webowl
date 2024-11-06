b2002f3791cb002ca0c19f414652b73122f650bb
RaoufJB <165310940+RaoufJb@users.noreply.github.com>
2024-11-06 16:35:58 +0100

    init web owl app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import "./index.html";
-import testModule from "./scripts/module-a";
+import { Component, useState, mount, xml } from "@odoo/owl";
 
-function main() {
-    console.log("boilerplate-webpack-babel");
-    testModule.test();
+class Counter extends Component {
+  static template = xml`
+    <button t-on-click="() => state.value = state.value + props.increment">
+      Click Me! [<t t-esc="state.value"/>]
+    </button>`;
+
+  state = useState({ value: 0 });
 }
 
-main();
+class Root extends Component {
+  static template = xml`
+    <span>Hello Owl</span><br/><br/>
+    <Counter increment="2"/>`;
+
+  static components = { Counter };
+}
+
+mount(Root, document.body);
+
